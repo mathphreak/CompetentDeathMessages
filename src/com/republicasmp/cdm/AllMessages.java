3fc2db1b2d69935ef5d823e3ad37a7379f67b144
Matt H <mathphreak@gmail.com>
2013-03-30 19:40:04 -0600

    Clarify AllMessages.java
    so sicknote can edit it without having to read my mind too much

--- src/com/republicasmp/cdm/AllMessages.java
+++ src/com/republicasmp/cdm/AllMessages.java
@@ -6,16 +6,26 @@ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
 public abstract class AllMessages {
 	public final static Message allMessages[] = {
-		// thanks for these, Sicknote!
+		// the way this works is:
+		// new Message(template, player damage cause, entity type that killed the player, material)
+		// the material could be a block or a held item
+		// so to keep that from becoming an issue, use EntityType.PLAYER as the entity type for things where
+		// a player killed another player
+		// don't forget commas after each one except the last one
+		
+		// thanks for these, sicknote!
 		new Message("Cleanup on aisle 12, we seem to have spilled some <victim>", DamageCause.FALL, null, null),
 		new Message("<victim> can't break the laws of physics", DamageCause.FALL, null, null),
 		new Message("<victim> couldn't keep it up", DamageCause.FALL, null, null),
 		new Message("<victim> forgot to drink Red Bull", DamageCause.FALL, null, null),
+		
 		new Message("<killer>'s rod was too much for <victim> to handle", null, Material.STICK, EntityType.PLAYER),
+		
 		new Message("<victim>'s relationship with the squid could never last", DamageCause.DROWNING, null, null),
 		new Message("<victim> drowned", DamageCause.DROWNING, null, null),
 		new Message("<victim> forgot how to swim", DamageCause.DROWNING, null, null),
 		new Message("<victim> must have tried on concrete shoes", DamageCause.DROWNING, null, null),
+		
 		// ...and now for mathphreak's
 		new Message("<victim> was left for dead", null, null, EntityType.ZOMBIE),
 		new Message("The cacti ate <victim>", null, Material.CACTUS, null)
